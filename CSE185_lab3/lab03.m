9d40439ef68d44de6dcaedcbdfb95d6fef196e16
Charles <cchung28@ucmerced.edu>
2019-02-12 01:03:56 -0800

    progress for lab3

--- CSE185_lab3/lab03.m
+++ CSE185_lab3/lab03.m
@@ -4,16 +4,26 @@ img2 = im2double(imread('lena.jpg'));
 % 
 % 
 %% Median filter
-% patch_size = [3, 3];
-% % patch_size = [5, 5];
+patch_size = [3, 3];
+patch_size1= [5, 5];
 
-
+img_median = median_filter(img1, patch_size);
+img_median1 = median_filter(img1, patch_size1);
+figure, imshow(img_median);
+figure, imshow(img_median1);
+imwrite(img_median, 'median_3.jpg');
+imwrite(img_median1, 'median_5.jpg');
 
 %% Sobel filter
-% H = [1, 2, 1; 0, 0, 0; -1, -2, -1]; % horizontal edge
-% %H = [1, 0, -1; 2, 0, -2; 1, 0, -1]; % vertical edge
+ H = [1, 2, 1; 0, 0, 0; -1, -2, -1]; % horizontal edge
+ h = [1, 0, -1; 2, 0, -2; 1, 0, -1]; % vertical edge
 
-
+img_sobel = sobel_filter(img1, H);
+img_sobel1 = sobel_filter(img1, h);
+figure, imshow(img_sobel);
+figure, imshow(img_sobel1);
+imwrite(img_sobel, 'sobel_h.jpg');
+imwrite(img_sobel1, 'sobel_v.jpg');
 
 
 %% Gaussian filter
--- CSE185_lab3/median_filter.m
+++ CSE185_lab3/median_filter.m
@@ -1,6 +1,23 @@
 function img_filtered = median_filter(img, patch_size)
 
     % your code here
+    I2 = zeros(size(img));
+    var = floor(patch_size(1)/2);
     
+    for u = 1 + var: size(img, 2)- var
+        for v = 1 + var: size(img, 1)- var
+            
+            x1 = u - var; x2 = u + var;
+            y1 = v - var; y2 = v + var;
+            
+            patch = img(y1:y2, x1:x2);
+            
+            patch = patch(:);
+            value = median(patch);
+            I2(v, u) = value;
+        end
+    end
     
+    img_filtered = I2;
 end
+    
