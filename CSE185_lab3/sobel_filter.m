121838e4b7dab08f240c59f01946ecdd1146595d
Charles <cchung28@ucmerced.edu>
2019-02-25 16:57:29 -0800

    fixes

--- CSE185_lab3/sobel_filter.m
+++ CSE185_lab3/sobel_filter.m
@@ -4,8 +4,8 @@ function img_filtered = sobel_filter(img, kernel)
     I2 = zeros(size(img));
     var = 1;
     
-    for u = 2 + var: size(img, 2)- var
-        for v = 2 + var: size(img, 1)- var
+    for u = 2 + var: size(img, 2) - var
+        for v = 2 + var: size(img, 1) - var
             patch = sum(sum(kernel.*img(v-1: v+1, u-1: u+1)));
             I2(v, u) = patch;
         end
